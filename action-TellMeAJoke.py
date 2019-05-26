28f7395e904e9c08e705b4c20334fa340b8e7e29
aeisele <andre.eisele@gmail.com>
2019-05-26 18:22:20 +0200

    JokeTeller implemented

--- action-TellMeAJoke.py
+++ action-TellMeAJoke.py
@@ -1,7 +1,5 @@
 from snipsTools import SnipsConfigParser
 from hermes_python.hermes import Hermes
-from hermes_python.ontology import *
-import io
 import requests
 from bs4 import BeautifulSoup
 
@@ -30,7 +28,7 @@ class JokeTeller(object):
         self.start_blocking()
 
     # --> Sub callback function, one per intent
-    def tellMeAJoke_callback(self, hermes, intent_message):
+    def tellmejoke_callback(self, hermes, intent_message):
         # terminate the session first if not continue
         hermes.publish_end_session(intent_message.session_id, "")
 
@@ -51,7 +49,7 @@ class JokeTeller(object):
     def master_intent_callback(self,hermes, intent_message):
         coming_intent = intent_message.intent.intent_name
         if coming_intent == 'tellMeAJoke':
-            self.tellMeAJoke_callback(hermes, intent_message)
+            self.tellmejoke_callback(hermes, intent_message)
 
         # more callback and if condition goes here...
 
@@ -59,6 +57,7 @@ class JokeTeller(object):
     def start_blocking(self):
         with Hermes(MQTT_ADDR) as h:
             h.subscribe_intents(self.master_intent_callback).start()
+
 
 if __name__ == "__main__":
     JokeTeller()
