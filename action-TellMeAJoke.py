d568dd062d8e47b6db89d4a1ad77864357952d2c
Andre Eisele <andre.eisele@gmail.com>
2019-09-21 11:01:39 +0200

    Add category and drum.wav file

--- action-TellMeAJoke.py
+++ action-TellMeAJoke.py
@@ -1,4 +1,6 @@
 from hermes_python.hermes import Hermes
+from hermes_python.ontology.tts import RegisterSoundMessage
+from builtins import bytearray
 import requests
 from bs4 import BeautifulSoup
 
@@ -9,7 +11,6 @@ MQTT_IP_ADDR = "fanrio"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 INTENT = "andreis:tellmeajocke"
-
 
 class JokeTeller(object):
     """Class used to wrap action code with mqtt connection
@@ -27,21 +28,38 @@ class JokeTeller(object):
         # start listening to MQTT
         self.start_blocking()
 
+
+    def read_wav_data():
+        with open('joke_drum', 'rb') as f:
+            read_data = f.read()
+        return bytearray(read_data)
+
+
     # --> Sub callback function, one per intent
     def tellmejoke_callback(self, hermes, intent_message):
         # terminate the session first if not continue
-        hermes.publish_end_session(intent_message.session_id, "")
+        
 
-        page = requests.get("https://witze.at/zufallswitz")
+        if len(intent_message.slots) == 0:
+            hermes.publish_continue_session( intent_message.session_id, 'Aus welcher Kategorie möchtest Du denn einen Witz hören?', "JokeTeller")
+        elif intent_message.slots.joketype.first().value == 'egal':
+            print('egal')
+            page = requests.get("https://witze.at/zufallswitz")
+        else:
+            page = requests.get("https://witze.at/zufallswitz")
+            print(intent_message.slots.joketype.first().value )
+
+
+       
         soap = BeautifulSoup(page.content, 'html.parser')
 
-        joke = soap.find('article').find('p').get_text()
+        joke = soap.find('article').find('p').get_text() + ' ... [[sound:joke_drum]]'
 
         # action code goes here...
         print("[Received] intent: {}".format(intent_message.intent.intent_name))
 
         # if need to speak the execution result by tts
-        hermes.publish_start_session_notification(intent_message.site_id, joke, "JokeTeller")
+        hermes.publish_end_session(intent_message.site_id, joke, "JokeTeller")
 
     # More callback function goes here...
 
@@ -55,8 +73,12 @@ class JokeTeller(object):
 
     # --> Register callback function and start MQTT
     def start_blocking(self):
+        sound_ba = self.read_wav_data( )
+        sound = RegisterSoundMessage("joke_drum",sound_ba)
         with Hermes(MQTT_ADDR) as h:
-            h.subscribe_intent(INTENT, self.master_intent_callback).start()
+            h.register_sound(sound)\
+             .subscribe_intent(INTENT, self.master_intent_callback)\
+             .start()
 
 
 if __name__ == "__main__":
